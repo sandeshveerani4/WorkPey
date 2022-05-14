f0770806a0b597c2f40755165e3ea9e4fa29f6ff
sandeshveerani4 <sandeshveerani4@gmail.com>
2022-05-14 22:47:22 +0500

    master

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import Posts from "./components/Posts/Posts";
+export default function Example() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-white dark:bg-dark">
+      <h1 className="text-3xl font-bold">Hello</h1>
+      <Posts />
     </div>
   );
 }
-
-export default App;
--- server/index.js
+++ server/index.js
@@ -2,3 +2,26 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+//routes
+import postRoutes from "./routes/posts.js";
+//end routes
+
+const app = express();
+app.use("/posts", postRoutes);
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URI =
+  "mongodb+srv://workpey:Hf0rF7eB4apL663B@cluster0.v4vki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(CONNECTION_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
